67ce9ae865b8bc646899ff54c35eda88d49954a2
Luciana Serrão <luliserrao@eng.ci.ufpb.br>
2021-03-19 09:11:35 -0300

    Delete src directory

--- src/database/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/noderest', { useMongoCliente: true});
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
